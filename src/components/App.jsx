7ec98a64d33dbd6e183e8425b9d63e9452359615
Andrey9019 <129278140+Andrey9019@users.noreply.github.com>
2023-10-16 20:47:31 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,10 +9,10 @@ export class App extends Component {
 
   state = {
 contacts: [
-    {id: 'id-1', firstName: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', firstName: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', firstName: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', firstName: 'Annie Copeland', number: '227-91-26'},
+    // {id: 'id-1', firstName: 'Rosie Simpson', number: '459-12-56'},
+    // {id: 'id-2', firstName: 'Hermione Kline', number: '443-89-12'},
+    // {id: 'id-3', firstName: 'Eden Clements', number: '645-17-79'},
+    // {id: 'id-4', firstName: 'Annie Copeland', number: '227-91-26'},
   ],  firstName: '',
     number: '',
     filter:'',
